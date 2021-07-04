09792a4e225634618dc43f6c82f6aa6ae465939d
Kyle Friel <businessfriel@gmail.com>
2021-07-04 11:31:32 -0400

    Express server functional. MongoDB Atlas connected.

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECT, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true,
+        });
+
+        mongoose.set("useFindAndModify", false);
+
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+import express from "express";
+// import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+connectDB();
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
