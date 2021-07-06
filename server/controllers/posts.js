b76b124dff7baf702331073e8358a3db7171a959
Kyle Friel <businessfriel@gmail.com>
2021-07-06 16:34:39 -0400

    Controllers likePost using $inc instead of previous method.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -49,10 +49,11 @@ export const likePost = async (req, res) => {
 
     if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id.");
 
-    const post = await PostMessage.findById(id);
+    // const post = await PostMessage.findById(_id);
     const updatedPost = await PostMessage.findByIdAndUpdate(
-        id,
-        { likeCount: post.likeCount + 1 },
+        _id,
+        // { likeCount: post.likeCount + 1 },
+        { $inc: { likeCount: 1 } },
         { new: true }
     );
 
